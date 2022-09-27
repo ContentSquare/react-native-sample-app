4a8f98d48bf57af3480e9dce26f5f17c9a7b5fd6
Fanny Tavart <fannyt@bam.tech>
2022-09-27 12:17:38 +0200

    feat: add ScreenViews navigation from Home

--- src/modules/Home/views/Home.tsx
+++ src/modules/Home/views/Home.tsx
@@ -1,23 +1,36 @@
 import React from 'react';
 import { FlatList, StyleSheet, View } from 'react-native';
+import { Screens } from '../../../app/navigation/Screens';
+import { useNavigation } from '../../../app/navigation/useNavigation';
 import { colors } from '../../../constants';
 import { ListItem } from '../../../shared/ListItem';
 import { Separator } from '../../../shared/Separator';
 
 const screensTitle = [
-  'Screen views',
-  'Privacy',
-  'Dynamic variables',
-  'Transactions',
-  'Masking',
+  { title: 'Screen views', navigationScreen: Screens.SCREEN_VIEWS },
+  { title: 'Privacy' },
+  { title: 'Dynamic variables' },
+  { title: 'Transactions' },
+  { title: 'Masking' },
 ];
 
 export const Home: React.FunctionComponent = () => {
+  const { navigate } = useNavigation();
+
   return (
     <View style={styles.container}>
       <FlatList
         data={screensTitle}
-        renderItem={item => <ListItem label={item.item} />}
+        renderItem={({ item }) => (
+          <ListItem
+            label={item.title}
+            onPress={() =>
+              item.navigationScreen
+                ? navigate(item.navigationScreen)
+                : undefined
+            }
+          />
+        )}
         ItemSeparatorComponent={Separator}
       />
     </View>
