3513e13571001ba0154a0b96da3f656e67af8e06
Gguigre <gui.gremi@gmail.com>
2022-10-03 15:43:05 +0200

    feat: remove optin button

--- src/modules/Home/views/Home.tsx
+++ src/modules/Home/views/Home.tsx
@@ -1,6 +1,5 @@
-import Contentsquare from '@contentsquare/react-native-bridge';
 import React from 'react';
-import { Button, FlatList, StyleSheet, View } from 'react-native';
+import { FlatList, StyleSheet, View } from 'react-native';
 import { colors } from '../../../constants';
 import { ListItem } from '../../../shared/ListItem';
 import { Separator } from '../../../shared/Separator';
@@ -17,12 +16,6 @@ export const Home: React.FunctionComponent = () => {
           <ListItem label={item.title} onPress={() => onListItemPress(item)} />
         )}
         ItemSeparatorComponent={Separator}
-      />
-      <Button
-        title="optin"
-        onPress={() => {
-          Contentsquare.optIn();
-        }}
       />
     </View>
   );
