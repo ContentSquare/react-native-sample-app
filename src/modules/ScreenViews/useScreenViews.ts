75d144cab11d060ffdfae23690568d7d15fdb205
Fanny Tavart <fannyt@bam.tech>
2022-10-03 18:09:38 +0200

    ✨ Feat: add navigation to BasicScreenView

--- src/modules/ScreenViews/useScreenViews.ts
+++ src/modules/ScreenViews/useScreenViews.ts
@@ -1,8 +1,12 @@
+import { Screens } from '../../app/navigation/Screens';
 import { ScreenConfig } from '../../shared/views/components/NavigationList/NavigationList';
 
 export const useScreenViews = () => {
   const screensConfig: ScreenConfig[] = [
-    { title: 'Default implementation' },
+    {
+      title: 'Default implementation',
+      navigationScreen: Screens.BASIC_SCREEN_VIEW,
+    },
     { title: 'Storyboard controllers' },
     { title: 'Paged scroll views' },
     { title: 'Modal dismissals' },
