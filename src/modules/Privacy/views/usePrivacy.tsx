3465e2c414f38f67a849da1492980fc567763397
Gguigre <gui.gremi@gmail.com>
2022-10-04 10:08:50 +0200

    ✨ Feat: add screenview for privacy screen

--- src/modules/Privacy/views/usePrivacy.tsx
+++ src/modules/Privacy/views/usePrivacy.tsx
@@ -1,7 +1,13 @@
+import Contentsquare from '@contentsquare/react-native-bridge';
+import { useEffect } from 'react';
 import { usePrivacyManagerModal } from '../../../shared/views/PrivacyManager/usePrivacyManagerModal';
 
 export const usePrivacy = () => {
   const { setIsPrivacyManagerVisible } = usePrivacyManagerModal();
+
+  useEffect(() => {
+    Contentsquare.send('Privacy');
+  }, []);
 
   const showPrivacyManager = () => {
     setIsPrivacyManagerVisible(true);
