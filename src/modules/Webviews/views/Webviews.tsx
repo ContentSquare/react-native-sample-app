44e7ed413b41d328508140eba8f0b035fc3d2e73
Gguigre <gui.gremi@gmail.com>
2022-10-06 10:46:31 +0200

    ✨ Feat: connect webview events to js bridge

--- src/modules/Webviews/views/Webviews.tsx
+++ src/modules/Webviews/views/Webviews.tsx
@@ -1,7 +1,26 @@
-import React, { FunctionComponent } from 'react';
+import Contentsquare from '@contentsquare/react-native-bridge';
+import React, { FunctionComponent, useEffect, useRef } from 'react';
 import { WebView } from 'react-native-webview';
 import { htmlPage } from './page';
 
 export const Webviews: FunctionComponent = () => {
-  return <WebView source={{ html: htmlPage }} />;
+  const webViewRef = useRef<WebView>(null);
+
+  useEffect(() => {
+    // When useEffect is called, we inject our WebView.
+    // We do not have state changes in this component, so we do not protect the call.
+
+    Contentsquare.send('Webviews');
+
+    Contentsquare.injectWebView(webViewRef);
+    webViewRef.current;
+
+    return () => {
+      // We are removing the injection in the return function, which is run when this component unmounts.
+      Contentsquare.removeWebViewInjection(webViewRef);
+    };
+  }, [webViewRef]);
+
+  // Contentsquare sdk is not compatible with react-native-webview >= 11.21
+  return <WebView source={{ html: htmlPage }} ref={webViewRef} />;
 };
--- src/modules/Webviews/views/page.ts
+++ src/modules/Webviews/views/page.ts
@@ -4,13 +4,27 @@ export const htmlPage = `
     <meta charset="utf-8">
     <title>WebView Tracking Tag - Contentsquare Technical Documentation</title>
     <script type="text/javascript">
-      (function() {
-        var mt = document.createElement("script"); mt.type = "text/javascript"; mt.async = true;
-        mt.src = "//t.contentsquare.net/wvt/web-view.js";
-        document.getElementsByTagName("head")[0].appendChild(mt);
-        }
-        )();
+      function sendScreenView() {
+        window.cs_wvt = window.cs_wvt || [];
+        const result = window.cs_wvt.push(['trackPageview', 'My Custom Page Title']) // triggers a pageview event with "My Custom Page Title" as the page title
+      }
+
+      function sendTransaction() {
+        window.cs_wvt = window.cs_wvt || [];
+        const result = window.cs_wvt.push(['trackTransaction', { value: 1000, currency: 'EUR', id: 'my-transaction-id'}]) 
+      }
+
+      function optOut() {
+        window.cs_wvt = window.cs_wvt || [];
+        const result = window.cs_wvt.push(['optout']);
+      }
+
+      function optIn() {
+        window.cs_wvt = window.cs_wvt || [];
+        const result = window.cs_wvt.push(['optin']);
+      }
     </script>
+
   </head>
   <body>
     <div class="container">
@@ -23,16 +37,35 @@ export const htmlPage = `
           between your WebView content and the native SDK.</p>
           <h2 style="font-size:22pt">Sending webview events</h2>
           <div>
-            <button>
+            <button onclick="optIn()">
+              <h3 style="font-size:20pt">Opt-in</h3>
+            </button>
+            <button onclick="optOut()">
+              <h3 style="font-size:20pt">Opt-out</h3>
+            </button>
+          </div>
+
+          <h2 style="font-size:22pt">Sending webview events</h2>
+          <div>
+            <button onclick="sendScreenView()">
               <h3 style="font-size:20pt">Send event</h3>
             </button>
-            <button>
+            <button onclick="sendTransaction()">
               <h3 style="font-size:20pt">Send 10$ transaction</h3>
             </button>
           </div>
         </div>
       </section>
     </div>
+
+    <script type="text/javascript">
+    (function() {
+      var mt = document.createElement("script"); mt.type = "text/javascript"; mt.async = true;
+      mt.src = "https://t.contentsquare.net/wvt/web-view.js";
+      document.getElementsByTagName("head")[0].appendChild(mt);
+    })();
+    </script>
+
   </body>
 </html>
 `;
