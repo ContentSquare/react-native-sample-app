a09e691251ca5c17fd3fb812b2439e9fce0f7eb1
Fanny Tavart <fannyt@bam.tech>
2022-10-06 17:17:45 +0200

    ✨ Feat: update Transactions feature

--- src/modules/Transactions/Transactions.tsx
+++ src/modules/Transactions/Transactions.tsx
@@ -1,41 +1,86 @@
 import React from 'react';
-import { Button, StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
 import { colors, gridUnit } from '../../constants';
+import { PriceButton } from '../../shared/PriceButton';
+import { Spacer } from '../../shared/Spacer';
+import { CartLine } from './components/CartLine';
 import {
-  ITEM_NAME,
-  ITEM_PRICE,
-  ITEM_PRICE_CURRENCY,
+  ITEMS_CURRENCY,
+  ITEM_1_NAME,
+  ITEM_1_PRICE,
+  ITEM_2_NAME,
+  ITEM_2_PRICE,
   useTransactions,
 } from './useTransactions';
 
 export const Transactions: React.FunctionComponent = () => {
   const {
-    numberOfItems,
-    transactionNumber,
+    total,
     onAddItemButtonPress,
     onValidateButtonPress,
+    numberOfItems1,
+    numberOfItems2,
   } = useTransactions();
 
   return (
     <View style={styles.container}>
       <Text style={styles.text}>
         This showcases how to track purchases made by an user with
-        CustomerTransactions. Add items to the cart then press the validate
+        CustomerTransactions. Add items to the cart then press the confirm
         button to send the transaction.
       </Text>
-      <Text
-        style={
-          styles.text
-        }>{`Item: ${ITEM_NAME} | Unit price: ${ITEM_PRICE} EUR (${ITEM_PRICE_CURRENCY})`}</Text>
-      <Text style={styles.text}>{`${numberOfItems} x ${ITEM_NAME}: TOTAL = ${
-        numberOfItems * ITEM_PRICE
-      } EUR (${ITEM_PRICE_CURRENCY})`}</Text>
-      <Text
-        style={styles.text}>{`Transaction number ${transactionNumber}`}</Text>
-      <View style={styles.buttonsView}>
-        <Button title="Add item" onPress={onAddItemButtonPress} />
-        <Button title="Validate cart" onPress={onValidateButtonPress} />
+      <Spacer height={gridUnit} />
+      <PriceButton
+        label={ITEM_1_NAME}
+        price={ITEM_1_PRICE}
+        currency={ITEMS_CURRENCY}
+        onPress={() => onAddItemButtonPress(ITEM_1_PRICE)}
+      />
+      <Spacer height={gridUnit} />
+      <PriceButton
+        label={ITEM_2_NAME}
+        price={ITEM_2_PRICE}
+        currency={ITEMS_CURRENCY}
+        onPress={() => onAddItemButtonPress(ITEM_2_PRICE)}
+      />
+      <Spacer height={gridUnit * 2} />
+      <View style={styles.cart}>
+        {numberOfItems1 === 0 && numberOfItems2 === 0 ? (
+          <View style={styles.emptyCartView}>
+            <Text>Your cart is empty</Text>
+          </View>
+        ) : (
+          <>
+            <CartLine
+              quantity={numberOfItems1}
+              itemName={ITEM_1_NAME}
+              unitPrice={ITEM_1_PRICE}
+              currency={ITEMS_CURRENCY}
+            />
+            <CartLine
+              quantity={numberOfItems2}
+              itemName={ITEM_2_NAME}
+              unitPrice={ITEM_2_PRICE}
+              currency={ITEMS_CURRENCY}
+            />
+          </>
+        )}
       </View>
+      <Spacer height={gridUnit * 2} />
+      <PriceButton
+        label="Confirm order (identified)"
+        price={total}
+        currency={ITEMS_CURRENCY}
+        onPress={() => onValidateButtonPress(true)}
+      />
+      <Spacer height={gridUnit} />
+      <PriceButton
+        label="Confirm order (unidentified)"
+        price={total}
+        currency={ITEMS_CURRENCY}
+        onPress={() => onValidateButtonPress(false)}
+      />
+      <Spacer height={gridUnit * 2} />
     </View>
   );
 };
@@ -44,14 +89,21 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: colors.white,
-    justifyContent: 'center',
     padding: gridUnit * 2,
   },
   text: {
     paddingVertical: gridUnit,
     textAlign: 'center',
   },
-  buttonsView: {
-    paddingTop: gridUnit * 3,
+  cart: {
+    flex: 1,
+    backgroundColor: colors.sand,
+    borderRadius: gridUnit,
+    padding: gridUnit * 2,
+  },
+  emptyCartView: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
   },
 });
--- src/modules/Transactions/useTransactions.ts
+++ src/modules/Transactions/useTransactions.ts
@@ -1,33 +1,45 @@
 import Contentsquare, { Currency } from '@contentsquare/react-native-bridge';
 import { useState } from 'react';
 
-export const ITEM_NAME = 'Bottle of water';
-export const ITEM_PRICE = 2;
-export const ITEM_PRICE_CURRENCY = Currency.EUR;
+export const ITEM_1_NAME = 'Bottle of water';
+export const ITEM_1_PRICE = 2;
+
+export const ITEM_2_NAME = 'Bottle of iced tea';
+export const ITEM_2_PRICE = 3;
+
+export const ITEMS_CURRENCY = Currency.EUR;
 
 export const useTransactions = () => {
-  const [numberOfItems, setNumberOfItems] = useState(0);
   const [transactionNumber, setTransactionNumber] = useState(1);
+  const [cart, setCart] = useState<number[]>([]);
 
-  const onValidateButtonPress = () => {
-    // Sends transaction event when pressing the validate button
-    Contentsquare.sendTransaction(
-      numberOfItems * ITEM_PRICE,
-      ITEM_PRICE_CURRENCY,
-      `transaction#${transactionNumber}`,
-    );
-    setNumberOfItems(0);
-    setTransactionNumber(transactionNumber + 1);
+  const getTotal = () => {
+    return cart.reduce((partialSum, a) => partialSum + a, 0);
   };
 
-  const onAddItemButtonPress = () => {
-    setNumberOfItems(numberOfItems + 1);
+  const onValidateButtonPress = (isIdentified: boolean) => {
+    // Sends transaction event with an optional transactionID when pressing the validate button
+    Contentsquare.sendTransaction(
+      getTotal(),
+      ITEMS_CURRENCY,
+      isIdentified ? `transaction#${transactionNumber}` : undefined,
+    );
+    if (isIdentified) {
+      setTransactionNumber(transactionNumber + 1);
+    }
+    setCart([]);
+  };
+
+  const onAddItemButtonPress = (newItemPrice: number) => {
+    setCart([...cart, newItemPrice]);
   };
 
   return {
-    numberOfItems,
     transactionNumber,
     onAddItemButtonPress,
     onValidateButtonPress,
+    numberOfItems1: cart.filter(itemPrice => itemPrice === ITEM_1_PRICE).length,
+    numberOfItems2: cart.filter(itemPrice => itemPrice === ITEM_2_PRICE).length,
+    total: getTotal(),
   };
 };
