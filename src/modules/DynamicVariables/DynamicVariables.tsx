3c0fc3c98d9bb33aec6efdb08b3ea3b386a29f33
Fanny Tavart <fannyt@bam.tech>
2022-10-07 10:55:13 +0200

    ✨ Feat: create DynamicVariables page

---
+++ src/modules/DynamicVariables/DynamicVariables.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { Button, StyleSheet, Text, View } from 'react-native';
+import { colors, gridUnit } from '../../constants';
+
+export const DynamicVariables: React.FunctionComponent = () => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>
+        This showcases how to use dynamic variables to gather additional data
+        about the session, suc as which version of an A/B test the user saw, or
+        other relevant user data. Please refer to the logs to check the
+        variables being sent.
+      </Text>
+      <Button title="Send a number" />
+      <Button title="Send a string" />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: colors.white,
+    padding: gridUnit * 2,
+  },
+  text: {
+    textAlign: 'center',
+    paddingBottom: gridUnit * 2,
+  },
+});
