616117a390e6c9bb14d99ced51a9f96de0b7d98d
Fanny Tavart <fannyt@bam.tech>
2022-10-07 10:59:37 +0200

    🐛 Fix: remove opt in in Home

--- src/modules/Home/views/Home.tsx
+++ src/modules/Home/views/Home.tsx
@@ -1,13 +1,9 @@
-import Contentsquare from '@contentsquare/react-native-bridge';
 import React from 'react';
 import { NavigationList } from '../../../shared/views/NavigationList/NavigationList';
 import { useHome } from './useHome';
 
 export const Home: React.FunctionComponent = () => {
   const { screensConfig } = useHome();
-
-  // TODO: remove after merging RN6
-  Contentsquare.optIn();
 
   return <NavigationList screensConfig={screensConfig} />;
 };
