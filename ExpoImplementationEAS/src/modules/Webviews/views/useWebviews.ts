196a4b930443142f9854e1b0e7fae518d4a73d85
Fanny Tavart <fannyt@bam.tech>
2022-12-27 10:07:15 +0100

    feat: add useWebviews hook

---
+++ ExpoImplementationEAS/src/modules/Webviews/views/useWebviews.ts
@@ -0,0 +1,45 @@
+import Contentsquare from "@contentsquare/react-native-bridge";
+import { useLayoutEffect, useState } from "react";
+import { WebViewNavigationEvent } from "react-native-webview/lib/WebViewTypes";
+
+export const useWebviews = () => {
+    const [webViewNativeTag, setWebviewNativeTag] = useState<number>();
+    const [injectedWebViewTag, setInjectedWebViewTag] = useState<number>();
+  
+    const loadStart = (event: WebViewNavigationEvent) => {
+
+        console.log('start');
+        
+      // react-native-webview typing for nativeEvent.target is not correct
+      // @ts-ignore
+      const webViewTag = event.nativeEvent.target as number;
+      if (webViewTag) {
+        setWebviewNativeTag(webViewTag);
+      }
+    };
+  
+    const loadEnd = () => {
+      if (
+        webViewNativeTag !== undefined &&
+        webViewNativeTag !== injectedWebViewTag
+      ) {
+        // @ts-ignore
+        Contentsquare.injectWebView(webViewNativeTag);
+        setInjectedWebViewTag(webViewNativeTag);
+      }
+    };
+  
+    useLayoutEffect(() => {
+        console.log('useEffect');
+        
+      return () => {
+        if (webViewNativeTag !== undefined) {
+          // @ts-ignore
+          Contentsquare.removeWebViewInjection(webViewNativeTag);
+        }
+      };
+    }, [webViewNativeTag]);
+
+    return { loadStart, loadEnd }
+  
+}
