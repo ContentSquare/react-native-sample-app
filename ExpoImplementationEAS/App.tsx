14d713d1b481e76159b5a0105dafc46829375823
Fanny Tavart <fannyt@bam.tech>
2023-01-31 14:29:18 +0100

    add dynamic link

--- ExpoImplementationEAS/App.tsx
+++ ExpoImplementationEAS/App.tsx
@@ -2,22 +2,29 @@ import React, { useEffect } from "react";
 import { StyleSheet, Text, View } from "react-native";
 import { colors, gridUnit } from "./src/constants";
 import Contentsquare from "@contentsquare/react-native-bridge";
+import { useAppInit } from "./src/useAppInit";
 
 const App = () => {
+  const { isLoadingComplete } = useAppInit();
+
   useEffect(() => {
     Contentsquare.optIn();
     Contentsquare.send("Expo screen view");
   }, []);
 
-  return (
-    <View style={styles.container}>
-      <Text style={styles.text}>
-        This app showcases how to add the Contentsquare bridge in an Expo EAS
-        app. Please check the ClassicImplementation project for insight about
-        how to properly use the Contentsquare bridge in your app.{" "}
-      </Text>
-    </View>
-  );
+  if (!isLoadingComplete) {
+    return null;
+  } else {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.text}>
+          This app showcases how to add the Contentsquare bridge in an Expo EAS
+          app. Please check the ClassicImplementation project for insight about
+          how to properly use the Contentsquare bridge in your app.{" "}
+        </Text>
+      </View>
+    );
+  }
 };
 
 const styles = StyleSheet.create({
---
+++ ExpoImplementationEAS/src/useAppInit.ts
@@ -0,0 +1,37 @@
+import Contentsquare from '@contentsquare/react-native-bridge';
+import { useEffect, useState } from 'react';
+import { Linking } from 'react-native';
+
+export const useAppInit = () => {
+  const handleOpenURL = (event: { url: string }) => {
+    Contentsquare.handleUrl(event.url);
+  };
+
+  const [isLoadingComplete, setLoadingComplete] = useState(false);
+
+  // Contentsquare in-app link listener, see documentation https://docs.contentsquare.com/react-native/#1-call-the-react-native-api
+  useEffect(() => {
+    const loadResourcesAndDataAsync = async () => {
+      try {
+        Linking.getInitialURL()
+          .then(url => {
+            if (url) {
+              handleOpenURL({ url });
+            }
+          })
+          .catch(err => {
+            console.warn('An error occurred', err);
+          });
+        Linking.addEventListener('url', handleOpenURL);
+      } catch (e) {
+        // We might want to provide this error information to an error reporting service
+        console.warn(e);
+      } finally {
+        setLoadingComplete(true);
+      }
+    };
+    loadResourcesAndDataAsync();
+  }, []);
+
+  return { isLoadingComplete };
+};
