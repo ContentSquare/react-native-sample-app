08fe4587802ab4527561041e6c591b521b7c5a2d
Toni Vujevic <toni.v1988+git@gmail.com>
2023-09-19 15:57:03 +0200

    fixed in-app features, need to call RCTLinkingManager (#44)

--- ClassicImplementation/ios/ReactNativeSampleApp/AppDelegate.mm
+++ ClassicImplementation/ios/ReactNativeSampleApp/AppDelegate.mm
@@ -1,16 +1,17 @@
 #import "AppDelegate.h"
 
 #import <React/RCTBundleURLProvider.h>
+#import <React/RCTLinkingManager.h>
 
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
-   self.moduleName = @"ReactNativeSampleapp";
+  self.moduleName = @"ReactNativeSampleapp";
   // You can add your custom initial props in the dictionary below.
   // They will be passed down to the ViewController used by React Native.
   self.initialProps = @{};
-
+  
   return [super application:application didFinishLaunchingWithOptions:launchOptions];
 }
 
@@ -30,6 +31,13 @@
 - (BOOL)concurrentRootEnabled
 {
   return true;
+}
+
+- (BOOL)application:(UIApplication *)application
+            openURL:(NSURL *)url
+            options:(NSDictionary<UIApplicationOpenURLOptionsKey,id> *)options
+{
+  return [RCTLinkingManager application:application openURL:url options:options];
 }
 
 @end
