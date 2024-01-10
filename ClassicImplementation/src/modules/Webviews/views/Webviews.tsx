380fb5d05132d1d4537b1d9ffc2030c84e0c37be
Toni Vujevic <toni.v1988+git@gmail.com>
2024-01-10 12:25:14 +0100

    feat: updated CS SDK version to 4.0.1 (#66)
    * updated sdk version
    
    * updated in adobe example app

--- ClassicImplementation/src/modules/Webviews/views/Webviews.tsx
+++ ClassicImplementation/src/modules/Webviews/views/Webviews.tsx
@@ -1,16 +1,21 @@
+import { CSWebView } from '@contentsquare/react-native-bridge';
 import React from 'react';
 import { WebView } from 'react-native-webview';
-import { htmlPage } from './page';
-import { useWebviews } from './useWebviews';
 
 export const Webviews = () => {
-  const { loadStart, loadEnd } = useWebviews();
-
   return (
-    <WebView
-      onLoadStart={loadStart}
-      onLoadEnd={loadEnd}
-      source={{ html: htmlPage }}
+    <CSWebView
+      url="" // put your url here
+      renderWebView={(onLayout, webViewUrl) => {
+        return (
+          <WebView
+            onLayout={onLayout}
+            source={{
+              uri: webViewUrl,
+            }}
+          />
+        );
+      }}
     />
   );
 };
