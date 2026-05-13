bf697706ec17858a1df507cf0cbb8f3ed0f0c19f
Aldina Sculac <sculac.aldina@gmail.com>
2026-05-13 14:13:44 +0200

    chore: update classic implementation to RN 0.85 and CSQ 6.3.0

--- ClassicImplementation/src/shared/views/PrivacyManager/usePrivacyManager.ts
+++ ClassicImplementation/src/shared/views/PrivacyManager/usePrivacyManager.ts
@@ -1,10 +1,10 @@
-import {CSQ} from '@contentsquare/react-native-bridge';
+import { CSQ, StartConfig } from '@contentsquare/react-native-bridge';
 import AsyncStorage from '@react-native-async-storage/async-storage';
-import {useEffect, useState} from 'react';
-import {usePrivacyManagerModal} from './usePrivacyManagerModal';
+import { useEffect, useState } from 'react';
+import { usePrivacyManagerModal } from './usePrivacyManagerModal';
 
 export const usePrivacyManager = () => {
-  const {isPrivacyManagerVisible, setIsPrivacyManagerVisible} =
+  const { isPrivacyManagerVisible, setIsPrivacyManagerVisible } =
     usePrivacyManagerModal();
 
   const [isContentSquareActive, setIsContentSquareActive] = useState(false);
@@ -26,7 +26,7 @@ export const usePrivacyManager = () => {
 
     // Here we register the user consent for tracking
     // the others SDK (if any) should also be activated
-    CSQ.start();
+    CSQ.start(StartConfig.dxa());
     CSQ.optIn();
     AsyncStorage.setItem('PRIVACY_CONSENT', 'true');
     setIsPrivacyManagerVisible(false);
@@ -44,7 +44,7 @@ export const usePrivacyManager = () => {
 
   const onSubmit = () => {
     if (isContentSquareActive) {
-      CSQ.start();
+      CSQ.start(StartConfig.dxa());
       CSQ.optIn();
     } else {
       CSQ.optOut();
